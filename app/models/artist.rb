3c37c56eca9b7820e7b24bbcc15e1ef8dbbaaa1a
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-06-11 12:14:50 -0400

    Update artist.rb

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,43 @@
 class Artist
-
   attr_reader :name, :years_experience
+  @@all=[]
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def painting
+    Painting.all.select{|painting|
+    painting.artist == self}
+  end
+
+  def galleries
+      #self.paintings.map {|galley_ins| }
+      painting.map {|galley_ins|
+      galley_ins.gallery}
+    end
+
+  def cities
+      galleries.map {|galley_ins|
+      galley_ins.city}
+    end
+
+  def self.total_experience
+     self.all.sum{|artist| artist.years_experience}
+    end
+
+  #Returns an instance of the artist with the highest amount of paintings per year of experience.
+  def self.most_prolific
+    self.all.max_by{|artist| artist.painting.count/artist.years_experience}
+  end
+  
+  def self.create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+end
 end
