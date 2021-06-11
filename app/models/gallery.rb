9f21dc07f33f170d08f9a971b49f2954921e9189
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2021-06-11 12:17:19 -0400

    Update gallery.rb

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -17,16 +17,12 @@ class Gallery
       painting.gallery == self}
     end
 
-#Returns an array of all artists that have a painting in a gallery
-  
   def artists
     paintings.map do |artist_ins| 
     artist_ins.artist
     end.uniq
   end
 
-#Returns an array of the names of all artists that have a painting in a gallery
-  
 def artist_names
   artists.map {|artist_ins|
   artist_ins.name}.uniq
@@ -34,5 +30,7 @@ end
 
 def most_expensive_painting
   self.paintings.max_by{|painting| painting.price}
+end
+  end
 end
   end
